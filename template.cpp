bc6a2a51cc6916c174f90407c072dcdc305c5f71
Yahiro Wataru <wtr816@gmail.com>
2016-11-01 03:29:09 +0900

    template

---
+++ template.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long ll;
+
+#define MAX_N (10000000)
+
+int main() {
+
+  return 0;
+}
