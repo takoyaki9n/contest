6d0032817c7c3cf87112c9add969aee8af4b0e3d
Yahiro Wataru <wtr816@gmail.com>
2016-12-19 23:25:57 +0900

    ABC049 C Daydream - AC

---
+++ At_Coder/ABC/049/C_Daydream.cpp
@@ -0,0 +1,71 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+typedef long long int ll;
+typedef vector<ll> Vl;
+typedef vector<int> Vi;
+typedef pair<int, int> Pi;
+
+#define INF   (1e9 + 7)
+#define MAX_L (100000)
+
+string WORDS[4] = {"dream", "dreamer", "erase", "eraser"};
+string S;
+
+bool match(int n) {
+  if (n == S.size()) return true;
+
+  for (int i = 0; i < 4; i++) {
+    string w = WORDS[i];
+    bool flag = true;
+    for (int j = 0; j < w.size(); j++) {
+      if (n + j > S.size() || w[j] != S[n + j]) {
+        flag = false;
+        break;
+      }
+    }
+    if (flag) {
+      if (match(n + w.size())) return true;
+    }
+  }
+
+  return false;
+}
+
+void solve() {
+  if (match(0)) {
+    printf("YES\n");
+  } else {
+    printf("NO\n");
+  }
+}
+
+int main() {
+  cin >> S;
+
+  solve();
+  
+  return 0;
+}
