927b8a59356af7f61479798c18ed5e6821e8b6a5
Yahiro Wataru <wtr816@gmail.com>
2017-03-18 23:49:49 +0900

    ARC056 A Honest Or Dishonest - AC

---
+++ At_Coder/ABC/056/A_HonestOrDishonest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace A_HonestOrDishonest {
+    class Program {
+        static void Main(string[] args) {
+            // スペース区切りの整数の入力
+            string[] input = Console.ReadLine().Split(' ');
+            string a = input[0];
+            string b = input[1];
+            string ans = (a == b)? "H": "D";
+            Console.WriteLine(ans);
+        }
+    }
+}
