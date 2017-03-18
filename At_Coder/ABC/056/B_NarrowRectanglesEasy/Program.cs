92d16ed4df3e69924ee1c5d4691086744a1c4ec0
Yahiro Wataru <wtr816@gmail.com>
2017-03-18 23:50:55 +0900

    ARC056 B Narrow Rectangles Easy - AC

---
+++ At_Coder/ABC/056/B_NarrowRectanglesEasy/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace A_HonestOrDishonest {
+    class Program {
+        static void Main(string[] args) {
+            // スペース区切りの整数の入力
+            string[] input = Console.ReadLine().Split(' ');
+            int W = int.Parse(input[0]);
+            int a = int.Parse(input[1]);
+            int b = int.Parse(input[2]);
+            int ans = Math.Max(Math.Abs(a - b) - W, 0);
+            Console.WriteLine(ans);
+        }
+    }
+}
