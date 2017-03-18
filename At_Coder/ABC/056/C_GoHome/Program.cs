d3186636c5d6a8b8ef9ccac591a667a0994d952c
Yahiro Wataru <wtr816@gmail.com>
2017-03-18 23:52:13 +0900

    ARC056 C Go Home - AC

---
+++ At_Coder/ABC/056/C_GoHome/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace C_GoHome {
+    class Program {
+        static void Main(string[] args) {
+            int X = int.Parse(Console.ReadLine());
+            int i = 0, x = 0;
+            while(true) {
+                x += i;
+                if (x >= X) break;
+                i++;
+            }
+            Console.WriteLine(i);
+        }
+    }
+}
