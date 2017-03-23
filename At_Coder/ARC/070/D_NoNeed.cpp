b3647f6e1c3d5866f55c073002bc59620f91b6e5
Yahiro Wataru <wtr816@gmail.com>
2017-03-23 14:37:17 +0900

    ARC070 D No Need - AC O(NK)

--- At_Coder/ARC/070/D_NoNeed.cpp
+++ At_Coder/ARC/070/D_NoNeed.cpp
@@ -41,49 +41,34 @@ typedef pair<int, int> Pi;
 
 int N, K;
 int A[MAX_N];
-bool DP[MAX_N + 1][MAX_K];
-
-bool check(int x) {
-  for (int i = 0; i <= N; i++) 
-    for (int j = 0; j < K; j++) DP[i][j] = false;
-  DP[0][0] = true;
-
-  for (int i = 0; i < N; i++) {
-    for (int j = 0; j < K; j++) {
-      bool flag = DP[i][j];
-      int k = j - A[i];
-      if (i != x && k >= 0) flag |= DP[i][k];
-      DP[i + 1][j] = flag;
-    }
-  }
-
-  debug_printf("x = %d\n", x);
-  for (int i = 0; i <= N; i++) {
-    for (int j = 0; j < K; j++) debug_printf("%s ", DP[i][j]? "o": "x");
-    debug_printf("\n");
-  }
-  debug_printf("\n");
-
-  for (int k = max(0, K - A[x]); k < K; k++) 
-    if (DP[N][k]) return true;
-  
-  return false;
-}
+bool DP[MAX_K];
 
 void solve() {
   sort(A, A + N);
-  
-  int xl = -1, xr = N;    
-  while (xr - xl > 1) {
-    int xm = (xl + xr) / 2;
-    if (check(xm)) {
-      xr = xm;
-    } else {
-      xl = xm;
-    }
-  }  
+  ll sum = 0;
+  for (int i = 0; i < N; i++) sum += A[i];
+  for (int j = 0; j < K; j++) DP[j] = false;
+  DP[0] = true;
 
-  printf("%d\n", xr);
+  int ans = 0;
+  for (int i = N - 1; i >= 0; i--) {
+    bool no_need = true;
+    for (int j = 0; j < K; j++) {
+      if (DP[j] && j + sum >= K) {
+        no_need = false;
+        break;
+      }
+    }
+    if (no_need) ans++;
+    debug_printf("sum = %lld, a = %d\n", sum, A[i]);
+    for (int j = 0; j < K; j++) debug_printf("%s ", DP[j]? "o": "x");
+    debug_printf("\n");
+
+    for (int j = K - A[i] - 1; j >= 0; j--) DP[j + A[i]] |= DP[j];
+
+    sum -= A[i];
+  }
+  printf("%d\n", ans);
 }
 
 int main() {
