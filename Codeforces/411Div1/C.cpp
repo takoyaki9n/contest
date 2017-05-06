4d6e3c9d0153383429d151db1cf2c3cf98722683
Yahiro Wataru <wtr816@gmail.com>
2017-05-07 02:25:34 +0900

    Codeforces 411 Div1 C - AC #26906850

---
+++ Codeforces/411Div1/C.cpp
@@ -0,0 +1,62 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+#define DEBUG
+#ifdef DEBUG
+#define debug_printf printf
+#else
+#define debug_printf 1 ? 0 : printf
+#endif
+
+using namespace std;
+typedef long long int ll;
+typedef vector<ll> Vl;
+typedef vector<int> Vi;
+typedef pair<int, int> Pi;
+
+#define FST first
+#define SND second
+
+#define MAX_N   (100000)                  // 10^5
+// #define MAX_N   (1000000000)              // 10^9
+// #define MAX_N   (1000000000000000000)     // 10^18
+#define BASE    (1000000000 + 7)          // 10^9 + 7
+#define INFI    (1000000000 + 7)          // 10^9 + 7
+#define INFL    (1000000000000000000 + 7) // 10^18 + 7
+
+int N;
+
+ll min_cost() {
+  return (N - 1) / 2;
+}
+
+int main() {
+  cin >> N;
+
+  ll ans = min_cost();
+
+  cout << ans << endl;
+
+  return 0;
+}
