51ef30af69886deeaa328071c402657e8df62cc8
Yahiro Wataru <wtr816@gmail.com>
2017-05-07 01:49:01 +0900

    AGC014 D - AC #1268413

---
+++ At_Coder/AGC/014/D.cpp
@@ -0,0 +1,93 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#include <unordered_set>
+
+#define DEBUG
+#ifdef DEBUG
+#define debug_printf printf
+#else
+#define debug_printf 1 ? 0 : printf
+#endif
+
+using namespace std;
+typedef long long int ll;
+typedef vector<ll> Vl;
+typedef vector<int> Vi;
+typedef pair<int, int> Pi;
+typedef pair<ll, ll> Pl;
+
+#define FST first
+#define SND second
+
+#define MAX_N   (100000)                  // 10^5
+#define BASE    (1000000000 + 7)          // 10^9 + 7
+#define INFI    (1000000000 + 7)          // 10^9 + 7
+#define INFL    (1000000000000000000 + 7) // 10^18 + 7
+
+int N;
+set<int> G[MAX_N], V;
+
+void make_tree(int v) {
+  for (auto u: G[v]) {
+    G[u].erase(v);
+    make_tree(u);
+  }
+}
+
+void remove_match(int v) {
+  bool single = true;
+  for (auto u : G[v]) {
+    remove_match(u);
+    if (single && V.find(u) != V.end()) {
+      V.erase(u); V.erase(v);
+      single = false;
+    }
+  }
+}
+
+string winner() {
+  int root = 0;
+  make_tree(root);
+  remove_match(root);
+  return (V.size() == 0)? "Second": "First";
+}
+
+int main() {
+  cin >> N;
+
+  for (int i = 0; i < N - 1; i++) {
+    int a, b;
+    cin >> a >> b;
+    a--; b--;
+    G[a].insert(b);
+    G[b].insert(a);
+    V.insert(a);
+    V.insert(b);
+  }
+
+  string ans = winner();
+  cout << ans << endl;
+
+  return 0;
+}
